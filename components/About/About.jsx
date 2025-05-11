336d88be474601b15d9efcdfcaff41e70c05be6e
Khandaker-Mohyet <mohyet008@gmail.com>
2025-05-12 00:22:39 +0600

    update about section

--- components/About/About.jsx
+++ components/About/About.jsx
@@ -24,65 +24,26 @@ const About = () => {
             <div className="w-full flex-col justify-center lg:items-start items-center gap-10 inline-flex">
               <div className="w-full flex-col justify-center items-start gap-8 flex">
                 <div className="flex-col justify-start lg:items-start items-center gap-4 flex">
-                  <h6 className="text-neutral-500 dark:text-neutral-400 text-base font-normal leading-relaxed">
-                    About me
-                  </h6>
                   <div className="w-full flex-col justify-start lg:items-start items-center gap-3 flex">
                     <h2 className="text-4xl font-bold font-manrope leading-normal lg:text-start text-center">
-                      Our Journey in Web Development
+                      About me
                     </h2>
                     <p className="text-neutral-500 dark:text-neutral-400 text-base font-normal leading-relaxed lg:text-start text-center">
-                      From crafting dynamic user interfaces to building scalable
-                      backend solutions, we specialize in creating cutting-edge
-                      web applications that enhance user experience,
-                      performance, and functionality.
+                      I’m a passionate and results-driven Front-End Developer with a strong focus on building responsive, high-performance websites and web applications. I specialize in JavaScript, React.js, Node.js, HTML, and CSS, and I’m dedicated to delivering user-friendly, accessible, and visually appealing digital experiences. <br />
+                      <br />
+                      With a solid foundation in modern web technologies, I bring ideas to life through clean, efficient, and maintainable code. I believe great user experiences start with thoughtful design and end with seamless functionality—and that’s what I aim to deliver every time. <br />
+                      <br />
+                      As I grow in my career, I’ve expanded my knowledge to the Software Development level, actively working with Node.js to understand full-stack development and build more robust, scalable applications. <br />
+                      <br />
+                      Currently, I’m exploring and learning Redux, Next.js, Docker, and AWS to level up my development workflow, build production-ready applications, and gain experience in cloud deployment and DevOps tools. <br />
                     </p>
-                  </div>
-                </div>
-                <div className="w-full flex-col justify-center items-start gap-6 flex">
-                  <div className="w-full justify-start items-center gap-8 grid md:grid-cols-2 grid-cols-1">
-                    <div className="w-full h-full p-3.5 rounded-xl border border-gray-200 dark:border-neutral-700 hover:border-gray-400 dark:hover:border-neutral-400 transition-all duration-700 ease-in-out flex-col justify-start items-start gap-2.5 inline-flex">
-                      <h4 className="text-2xl font-bold font-manrope leading-9">
-                        Web Development
-                      </h4>
-                      <p className="text-gray-500 dark:text-gray-400 text-base font-normal leading-relaxed">
-                        Building modern, dynamic web applications.
-                      </p>
-                    </div>
-
-                    <div className="w-full h-full p-3.5 rounded-xl border border-gray-200 dark:border-neutral-700 hover:border-gray-400 dark:hover:border-neutral-400 transition-all duration-700 ease-in-out flex-col justify-start items-start gap-2.5 inline-flex">
-                      <h4 className="text-2xl font-bold font-manrope leading-9">
-                        20+ Projects
-                      </h4>
-                      <p className="text-gray-500 dark:text-gray-400 text-base font-normal leading-relaxed">
-                        Delivering High-Performance Websites & Applications
-                      </p>
-                    </div>
-                  </div>
-                  <div className="w-full h-full justify-start items-center gap-8 grid md:grid-cols-2 grid-cols-1">
-                    <div className="w-full p-3.5 rounded-xl border border-gray-200 dark:border-neutral-700 hover:border-gray-400 dark:hover:border-neutral-400 transition-all duration-700 ease-in-out flex-col justify-start items-start gap-2.5 inline-flex">
-                      <h4 className="text-2xl font-bold font-manrope leading-9">
-                        10+ Tech Stacks
-                      </h4>
-                      <p className="text-gray-500 dark:text-gray-400 text-base font-normal leading-relaxed">
-                        Expertise in React, Next.js, MongoDB, Node.js, and More
-                      </p>
-                    </div>
-                    <div className="w-full h-full p-3.5 rounded-xl border border-gray-200 dark:border-neutral-700 hover:border-gray-400 dark:hover:border-neutral-400 transition-all duration-700 ease-in-out flex-col justify-start items-start gap-2.5 inline-flex">
-                      <h4 className="text-2xl font-bold font-manrope leading-9">
-                        Secure & Scalable
-                      </h4>
-                      <p className="text-gray-500 dark:text-gray-400 text-base font-normal leading-relaxed">
-                        Fast, secure, and efficient web solutions.
-                      </p>
-                    </div>
                   </div>
                 </div>
               </div>
               <a href="#contact">
-              <Button>
-              Contact <GoArrowUpRight />
-            </Button>
+                <Button>
+                  Contact <GoArrowUpRight />
+                </Button>
               </a>
             </div>
             <div className="w-full lg:justify-end justify-center items-end flex">
